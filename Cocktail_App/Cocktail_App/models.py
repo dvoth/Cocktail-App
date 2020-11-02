3f76847b0472a656f7af7b392d000af7cbe0257c
Dalton Voth <dvoth@siue.edu>
2020-11-01 20:52:46 -0600

    Add fixture test data for recipes

--- Cocktail_App/Cocktail_App/models.py
+++ Cocktail_App/Cocktail_App/models.py
@@ -14,12 +14,13 @@ UNITS = [
     ('oz', 'ounce'),
     ('mg', 'milligram'),
     ('g', 'gram'),
-    ('kg', 'kilogram')
+    ('kg', 'kilogram'),
+    ('dashes', 'dashes')
 ]
 
 class IngredientType(models.Model):
     name = models.CharField(max_length=32)
-    parentType = models.ForeignKey('self', on_delete=models.CASCADE)
+    parentType = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
 
 class  Ingredient(models.Model):
     name = models.CharField(max_length=32)
@@ -30,10 +31,11 @@ class Recipe(models.Model):
     description = models.TextField(max_length = 1000)
 
 class RecipeIngredient(models.Model):
-    ingredient = models.ManyToManyField(Ingredient)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     measure = models.DecimalField(decimal_places=3, max_digits=5)
-    unit = models.CharField(max_length=32, choices=UNITS)
-    note = models.CharField(max_length=32)
+    unit = models.CharField(max_length=32, choices=UNITS, null=True)
+    note = models.CharField(max_length=32, null=True)
 
 class RecipeStep(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
